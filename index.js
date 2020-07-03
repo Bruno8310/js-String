fe7bd3e7c5d8818e2d033d0690c08b321d8d5596
Bruno8310 <gongl3730@gmail.com>
2020-07-03 18:13:10 +0800

    This is js-String

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
-
+name = name.toUpperCase();
+alert(name);
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+arr = sentence.split(' ');
+for(var i = 0; i < arr.length; i++) {
+    sentence = arr[i].substring(0, 1).toUpperCase() + arr[i].substring(1);
+}
+alert(sentence);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.substring(1);
+alert(money);
